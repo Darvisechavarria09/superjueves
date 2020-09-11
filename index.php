6d5c35796900194ce29f46b1ae671e7288692263
Darvisechavarria <yerikechavarria210@gmail.com>
2020-09-10 21:01:04 -0500

    se crea ejemplo 1

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+$producto1='arroz';
+$producto2='panela';
+$producto3='papa';
+$producto4='cerveza';
+$producto5='pasta';
+$producto6='atún';
+$producto7='frijoles';
+$producto8='carne';
+$producto9='arepas';
+$producto10='huevos';
+
+$precio1=1500;
+$precio2=7000;
+$precio3=8000;
+$precio4=30000;
+$precio5=2200;
+$precio6=9000;
+$precio7=4800;
+$precio8=25000;
+$precio9=10000;
+$precio10=14000;
+
+$productoEspecial='manzana';
+switch ($productoEspecial)
+{
+    case 'manzana':
+        echo('tiene un 10% de descuento');
+    break;
+
+    case 'banano':
+        echo('tiene un 5% de descuento');
+    break;
+
+    default:
+    echo('No tiene descuento');
+
+}
+if($productoEspecial=='manzana')
+{
+    echo('tiene un 10% de descuento');
+}
+else if($productoEspecial=='banano')
+{
+    echo('tiene un 5% de descuento');
+}
+else
+{
+    echo('No tiene descuento');
+}
+
+
+$total=$precio1+$precio2+$precio3+$precio4+$precio5+$precio6+$precio7+$precio8+$precio9+$precio10
+
+
+
+if($total > 80000)
+{
+    echo('saldo insuficiente')
+}
+else
+{
+    echo('Gracias por su compra')
+}
+
+
+
+
+?>
